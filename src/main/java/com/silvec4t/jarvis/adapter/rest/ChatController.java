0991183e05bb7f62cf6ed7442bfb138f2ca44ce6
Leonid <l.kiritschenko@gmail.com>
2024-08-15 22:18:13 +0200

    added api/v1 to path for future versioning

--- src/main/java/com/silvec4t/jarvis/adapter/rest/ChatController.java
+++ src/main/java/com/silvec4t/jarvis/adapter/rest/ChatController.java
@@ -3,9 +3,11 @@ package com.silvec4t.jarvis.adapter.rest;
 import com.silvec4t.jarvis.domain.ChatService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("api/v1")
 public class ChatController {
 
     @Autowired
